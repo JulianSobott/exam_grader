5c98bbcd184e532800e244bd6a6e553c087bcc5d
julian <julian.sobott@gmail.com>
2020-12-13 03:05:36 +0100

    Add sandbox implementation

---
+++ sandbox/src/main/java/MyClass1.java
@@ -0,0 +1,14 @@
+public class MyClass1 {
+
+    private int i = 20;
+    private String x = "Hello world";
+
+    public MyClass1() {
+        // Nothing
+    }
+
+    public int getNumber() {
+        return this.i;
+    }
+
+}
