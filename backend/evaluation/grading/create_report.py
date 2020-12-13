0573ca1dc9f566330b70abcee13f7f86c7420a7f
julian <julian.sobott@gmail.com>
2020-12-13 14:39:38 +0100

    refactor some grading code into new grading package

---
+++ backend/evaluation/grading/create_report.py
@@ -0,0 +1,7 @@
+from typing import Tuple, Optional, List
+
+from utils.p_types import error
+
+
+def create_report(submission_names: List[str] = None) -> Tuple[Optional[dict], error]:
+    pass  # TODO
