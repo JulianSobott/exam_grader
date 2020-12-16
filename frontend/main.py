5a9c217f43d80d102cd76017bb3476ec0e89600b
Sebastian Schirle <>
2020-12-16 22:51:26 +0100

    organize main, add path-selector
    add path-selector support
    add dummy data for correction
    clean up structure

--- frontend/main.py
+++ frontend/main.py
@@ -1,61 +1,100 @@
-# TODO work here
-
-from flask import Flask, render_template, render_template_string
-
+from flask import Flask, request, render_template, render_template_string
 from schema_classes import PrepareRequestBase, PreparePOST400Response, PreparePOST200Response
 
 app = Flask(__name__)
 
 
-@app.route("/dummy")
-def dummy():
-    var, err = PrepareRequestBase.post("/path/that/does/not/exist")
-    if err:
-        text = err
+@app.route("/", methods=['POST', 'GET'])
+def file_picker():
+    if request.method == 'POST':
+        path = request.form["path"]
+        # if path is empty
+        if path or path == "":
+            return render_template("selector.jinja2", text="filepath", status="Filepath must be specified!")
+        # FIXME missing parameter
+        # var, err = PrepareRequestBase.post(path)
+        # if err:
+        #     text = err
+        # else:
+        #     if isinstance(var, PreparePOST200Response):
+        #         text = var.file_errors
+        #     elif isinstance(var, PreparePOST400Response):
+        #         text = var.error
+        #     else:
+        #         text = var
+        return render_template("selector.jinja2", text=path, status=path)
     else:
-        if isinstance(var, PreparePOST200Response):
-            text = var.file_errors
-        elif isinstance(var, PreparePOST400Response):
-            text = var.error
-        else:
-            text = var
-    return render_template_string("{{ name }}", name=text)
-
-
-@app.route("/")
-def selctor():
-    return render_template("selector.jinja2")
-
-
-# def file_picker():
-#     var, err = PrepareRequestBase.post("/path/that/does/not/exist")
-#     if err:
-#         text = err
-#     else:
-#         if isinstance(var, PreparePOST200Response):
-#             text = var.file_errors
-#         elif isinstance(var, PreparePOST400Response):
-#             text = var.error
-#         else:
-#             text = var
-#     return render_template("correction.jinja2", name=text)
+        return render_template("selector.jinja2", text="filepath")
 
 
 @app.route("/correction")
 def correction():
     var = {
         "name": "Sebastian",
-        "matrikel": 81194,
-        "tested": 3,
-        "success": 2,
-        "failed": 1
+        "students_number": 81194,
+        "num_correct_subtasks": 2,
+        "num_subtasks": 4,
+        "points": 99,
+        "max_points": 100,
+        "step_failed": "Error 404",
+        "tasks": [{
+            "name": "Task 1",
+            "points": 3,
+            "max_points": 5,
+            "bookmarked": True,
+            "sub_tasks": [{
+                "name": "Subtask 1",
+                "description": "The task is to do this and that",
+                "points": 3.5,
+                "max_points": 5,
+                "bookmarked": True,
+                "code_snippets": [{
+                    "name": "Snippet 1",
+                    "class_name": "ClassTester.java",
+                    "found": True,
+                    "code": "This is the code for the task"
+                }],
+                "testcases": [{
+                    "name": "Assertion 1",
+                    "passed": False,
+                    "assertion": "The assertion to be passed"
+                }]
+            }, {
+                "name": "Subtask 2",
+                "description": "The task is to do this and that",
+                "points": 3.5,
+                "max_points": 5,
+                "bookmarked": True,
+                "code_snippets": [{
+                    "name": "Snippet 1",
+                    "class_name": "ClassTester.java",
+                    "found": True,
+                    "code": "This is the code for the task"
+                }],
+                "testcases": [{
+                    "name": "Assertion 1",
+                    "passed": False,
+                    "assertion": "The assertion to be passed"
+                }]
+            }]
+        }]
     }
-    return render_template("correction.jinja2", var=var)
+    return render_template("correction.jinja2", report=var)
 
 
 @app.route("/list")
-def list():
-    return render_template("list.jinja2")
+def exam_list():
+    dummy = [{
+        "name": "Student 1",
+        "passed": True,
+        "points": 30
+    }, {
+        "name": "Student 2",
+        "passed": False,
+        "points": 2
+    }]
+
+    return render_template("list.jinja2", reports=dummy)
 
 
 if __name__ == '__main__':
