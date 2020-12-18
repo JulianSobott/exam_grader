b9e5825f6e817c1c3697fd467842baeb192496ba
julian <julian.sobott@gmail.com>
2020-12-18 18:26:00 +0100

    Create js fetch methods from schemas

--- backend/evaluation/utils/schemas_creator.py
+++ backend/evaluation/utils/schemas_creator.py
@@ -1,25 +1,29 @@
-import json
 from pathlib import Path
 from typing import List
 
 from utils.web_requests_schema_parser.python import schema_to_python
+from utils.web_requests_schema_parser.to_js import schema_to_js
 
 
-def convert_all(src_folder: Path, dest_folder_json: Path, dest_folder_python: List[Path]):
-    for dest in dest_folder_python:
+def convert_all(src_folder: Path, dest_folder_js: List[Path], dest_folder_python: List[Path]):
+    for dest in dest_folder_python + dest_folder_js:
         dest.mkdir(parents=True, exist_ok=True)
     for file in src_folder.iterdir():
         with open(file, "r") as f:
             text = f.read()
-            python_content = schema_to_python(text)
             name = file.name.replace('.schema', '')
+
+            python_content = schema_to_python(text)
             for dest in dest_folder_python:
                 write_python_schema_file(name, dest, python_content)
 
+            js_content = schema_to_js(text)
+            for dest in dest_folder_js:
+                write_js_schema_file(name, dest, js_content)
 
-def write_json_schema_file(name: str, dest_folder: Path, schema: dict):
-    text = json.dumps(schema, indent=2)
-    write_file(f"{name}.schema.json", dest_folder, text)
+
+def write_js_schema_file(name: str, dest_folder: Path, text: str):
+    write_file(f"{name}_schema.js", dest_folder, text)
 
 
 def write_python_schema_file(name: str, dest_folder: Path, text: str):
@@ -37,6 +41,6 @@ if __name__ == '__main__':
     frontend_src = root_folder.joinpath("frontend")
     convert_all(
         root_folder.joinpath("schemas").joinpath("api_schemas"),
-        root_folder.joinpath("schemas").joinpath("gen_json"),
+        [frontend_src.joinpath("static").joinpath("js").joinpath("schema_classes")],
         [backend_src.joinpath("schema_classes"), frontend_src.joinpath("schema_classes")]
     )
