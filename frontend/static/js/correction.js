165cd5db64bfcfb951e639e429b121871242d2d1
Sebastian Schirle <>
2020-12-19 14:37:17 +0100

    push-requests correction.jinja2
    fix links in correction.jinja2
    improve unique id concept and function calls
    improve methods in correction_logic.jinja2

--- frontend/static/js/correction.js
+++ frontend/static/js/correction.js
@@ -1,68 +1,97 @@
 // later: add keyboard-shortcuts
-var url_base = "http://localhost:5000/api/v1"
 
-function bookmarkClick(id) {
-    // bookmark-submission-task-subtask
-    console.log(id)
-    const obj = {
-        "identifier":
-            {
-                elements: identifiers
-            },
-        bookmarked: bookmarked
-    }
-    fetch(url_base + "/grading/bookmark", {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(obj)
-    }).then(function (res) {
-        return res.json();
-    }).then(function (data) {
-        console.log(data)
+// toggle bookmark submission, task or subtask
+    // id = origin of the button click
+    // bookmark = current status of boolean bookmark
+function bookmarkClick(id, bookmark) {
+    // convert bookmark String to JS-boolean
+    if (bookmark === "True") bookmark = true;
+    else bookmark = false;
+    // split into id-sections (submission, task, subtask)
+    let elements = id.split("--")
+    console.log(elements);
+    // update toggled bookmark at server and show response
+    postBookmark({elements: elements}, bookmark).then( res => {
+        if (res.status_code === 200) {
+            // reload page to show the new icon
+            location.reload();
+        }
+        else {
+            console.log(res.error.err_msg)
+        }
     })
 }
 
+// when input changes: post points to server
+    // name_id = html-id of the origin input
+    // (must be striped before processing)
 function set_points(name_id) {
-    var points = document.getElementById(name_id).value;
-    var url = url_base + "/grading/points"
-    var params = "identifier=   &points=" + points;
-    var request = new XMLHttpRequest();
-    request.open("POST", url, true);
-    request.setRequestHeader("Points", "/correction");
-    request.send(params);
-    console.log(name_id + ": " + points);
+    // split into id-sections (submission, task, subtask)
+    let elements = name_id.split("__")[1].split("--")
+    console.log(elements)
+    // get points from input field
+    let points = parseFloat(document.getElementById(name_id).value);
+    // post points to server and show response
+    postPoints({elements: elements}, points).then(res => {
+        if (res.status_code === 200) {
+            console.log(name_id + ": " + points);
+        }
+        else {
+            console.error(res.error.err_msg);
+        }
+        console.log(res.status)
+    })
 }
 
+// when input changes: post comment to server
+    // name_id = html-id of the origin input
+    // (must be striped before processing)
 function set_comment(name_id) {
-    var comment = document.getElementById(name_id).value;
-    var url = url_base + "/grading/comment"
-    var params = "identifier=   &comment=" + comment;
-    var request = new XMLHttpRequest();
-    request.open("POST", url, true);
-    request.setRequestHeader("Comment", "/correction");
-    request.send(params);
-    console.log(name_id + ": " + comment);
+    // split into id-sections (submission, task, subtask)
+    let elements = name_id.split("__")[1].split("--")
+    console.log(elements)
+    // get comment from input field
+    let comment = document.getElementById(name_id).value;
+    // post comment to server and show response
+    postComment({elements: elements}, comment).then(res => {
+        console.log(res.status_code)
+        if (res.status_code === 200) {
+            console.log(name_id + ": " + comment);
+        }
+        else if (res.status_code === 404) {
+            console.log(res.err_msg)
+        }
+        else {
+            console.error(res.error.err_msg);
+        }
+    })
 }
 
-document.getElementById("finish_link").addEventListener("click", function finish() {
-    console.log("Finish")
-    var url = url_base + "/grading/status"
-    var status = "identifier=   &status=DONE";
-    var request = new XMLHttpRequest();
-    request.open("POST", url, true);
-    request.setRequestHeader("Finish", "/correction");
-    request.send(status);
-});
+// set submission status to DONE
+function finish(id) {
+    // post status to server and show response
+    postStatus({elements: [id]}, "DONE").then(res => {
+        console.log(res.status_code)
+        if (res.status_code === 200) {
+            console.log("status: DONE")
+        }
+        else {
+            console.error(res.error.err_msg);
+        }
+    })
+}
 
-document.getElementById("unfinish_link").addEventListener("click", function unfinish() {
-    console.log("Unfinish")
-    var url = url_base + "/grading/status"
-    var status = "identifier=   &points=ACTIVE";
-    var request = new XMLHttpRequest();
-    request.open("POST", url, true);
-    request.setRequestHeader("Unfinish", "/correction");
-    request.send(status);
-});
+// set submission status to ACTIVE
+    // also used to set the status from NOT_STARED to ACTIVE
+function unfinish(id) {
+    // post status to server and show response
+     postStatus({elements: [id]}, "ACTIVE").then(res => {
+        console.log(res.status_code)
+         if (res.status_code === 200) {
+            console.log("status: ACTIVE")
+        }
+        else {
+            console.error(res.error.err_msg);
+        }
+    })
+}
