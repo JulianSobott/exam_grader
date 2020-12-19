c07cbf23bd5bd676358fca5c110c14f39cbe63b3
julian <julian.sobott@gmail.com>
2020-12-19 12:45:22 +0100

    Create exel table function

--- backend/evaluation/grading/serialization.py
+++ backend/evaluation/grading/serialization.py
@@ -1,24 +1,25 @@
-import re
 from pathlib import Path
 
 import pandas as pd
 
+from config.exam_config import get_exam_config_else_raise
+from data.api import overview_data
 from utils.project_logging import get_logger
 
 logger = get_logger(__name__)
 
 
-def create_exel_table(path: Path = Path("OOP_ZK1_PunkteStudierende.xlsx")):
-    grading = load_gradings()  # TODO
-    data = {"MatNr": [], "Punkte": []}
+def create_exel_table(path: Path = None):
+    exam_name = get_exam_config_else_raise().name
+    if path is None:
+        path = Path(f"{exam_name}_gradings.xlsx")
+    grading = overview_data()
+    data = {"Name": [], "Punkte": [], "Canvas ID": []}
     for submission in grading.submissions:
-        submission_points = 0
-        for testsuite in submission.test_suites:
-            for testcase in testsuite.test_cases:
-                submission_points += testcase.points
-        mat_nr = "7" + re.findall(r"[0-9]{4}", submission.name)[0]
-        data["MatNr"].append(mat_nr)
-        data["Punkte"].append(submission_points)
+        data["Name"].append(submission.submission_name)
+        points = [e for e in submission.exam_points if e.exam_name == exam_name][0]
+        data["Punkte"].append(points)
+        data["Canvas ID"].append(submission.submission_id.replace(f"{submission.submission_name}-", ""))
 
     df = pd.DataFrame(data)
     df.to_excel(path, index=False)
